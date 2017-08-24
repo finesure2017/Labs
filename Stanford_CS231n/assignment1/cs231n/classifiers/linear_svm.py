cf8fab3a4f9e17027a6079106cc1f70fae390afc
scheeloong <scheeloong@gmail.com>
2017-08-24 04:48:46 -0400

    Implemented vectorized loss and vectorized gradient

--- Stanford_CS231n/assignment1/cs231n/classifiers/linear_svm.py
+++ Stanford_CS231n/assignment1/cs231n/classifiers/linear_svm.py
@@ -11,8 +11,7 @@ def svm_loss_naive(W, X, y, reg):
   of N examples.
 
   Inputs:
-  - W: A numpy array of shape (D, C) containing weights.
-  - X: A numpy array of shape (N, D) containing a minibatch of data.
+  - W: A numpy array of shape (D, C) containing weights.  - X: A numpy array of shape (N, D) containing a minibatch of data.
   - y: A numpy array of shape (N,) containing training labels; y[i] = c means
     that X[i] has label c, where 0 <= c < C.
   - reg: (float) regularization strength
@@ -81,25 +80,37 @@ def svm_loss_naive(W, X, y, reg):
 def svm_loss_vectorized(W, X, y, reg):
   """
   Structured SVM loss function, vectorized implementation.
+  - W: A numpy array of shape (D, C) containing weights.
+  - X: A numpy array of shape (N, D) containing a minibatch of data.
+  - y: A numpy array of shape (N,) containing training labels; y[i] = c means
+    that X[i] has label c, where 0 <= c < C.
 
   Inputs and outputs are the same as svm_loss_naive.
   """
   loss = 0.0
   dW = np.zeros(W.shape) # initialize the gradient as zero
-
   #############################################################################
-  # TODO:                                                                     #
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
-  pass
+
+  scores = np.dot(X, W)
+  numTrain = X.shape[0]
+  # Not sure if there's a better way to index, to create correctClassScore
+  correctClassScore = scores[np.arange(numTrain), y]
+  margin = scores - np.reshape(correctClassScore, (-1, 1)) + 1
+  lossPerClass = np.maximum(0, margin)
+  lossVector = np.sum(lossPerClass, axis=1)
+  totalLoss = np.average(lossVector)
+  totalLoss -= 1.0 # Deduct 1, which is the delta as added correct class score into margin
+  # Add regularization to the loss.
+  totalLoss += reg * np.sum(W * W)
+  loss = totalLoss
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
 
-
   #############################################################################
-  # TODO:                                                                     #
   # Implement a vectorized version of the gradient for the structured SVM     #
   # loss, storing the result in dW.                                           #
   #                                                                           #
@@ -107,9 +118,34 @@ def svm_loss_vectorized(W, X, y, reg):
   # to reuse some of the intermediate values that you used to compute the     #
   # loss.                                                                     #
   #############################################################################
-  pass
+  lossPerClass = np.maximum(0, margin)
+  # Make 0 for any where margin <= 0
+  # Make 1 for any where margin > 0
+  # Make -1 for any where it is the correct class
+  multiplication = np.zeros(lossPerClass.shape)
+  yCorrectClass = np.zeros(lossPerClass.shape)
+
+  posIndices = np.where(lossPerClass > 0)
+  # Set whatever that needs to create an update to 1
+  multiplication[posIndices] = 1.0
+  # However, should not update those position that required update 
+  # where the score computed was the correct class score. 
+  # Set all the correct class positions to 0
+  multiplication[np.arange(numTrain), y] = 0.0
+
+  # Sum across the rows to get the number of wrong instances for each training example
+  # (N, 1)
+  values = (-1.0 * np.sum(multiplication, axis=1))
+  # Set those rows to deduct x since it's correct examples
+  # note: This line does not handle the score computed was correct class score
+  # as you need it to all be 0.0 before calculating the values for below.
+  multiplication[np.arange(numTrain), y] = values
+  dW = np.dot(np.transpose(X), multiplication)
+  # Normalize the gradient
+  dW /= numTrain
+  # Add regularization gradient
+  dW += 2*reg*W
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
-
   return loss, dW
