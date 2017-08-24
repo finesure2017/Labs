eb2d80512163d834b6da390d427d09e9ab3f61aa
scheeloong <scheeloong@gmail.com>
2017-08-24 02:12:56 -0400

    Implemented gradient for linear svm before regularization

--- Stanford_CS231n/assignment1/cs231n/classifiers/linear_svm.py
+++ Stanford_CS231n/assignment1/cs231n/classifiers/linear_svm.py
@@ -1,6 +1,7 @@
 import numpy as np
 from random import shuffle
-from past.builtins import xrange
+
+import sys
 
 def svm_loss_naive(W, X, y, reg):
   """
@@ -26,31 +27,53 @@ def svm_loss_naive(W, X, y, reg):
   num_classes = W.shape[1]
   num_train = X.shape[0]
   loss = 0.0
-  for i in xrange(num_train):
+  for i in range(num_train):
     scores = X[i].dot(W)
     correct_class_score = scores[y[i]]
-    for j in xrange(num_classes):
+    for j in range(num_classes):
+    # for every training and class, 
+    # Loss equation will be: max(0, Sij - Sicorrectclassindex + 1)
+    # max(0, scores[j] - correct_class_score + 1)
+    # max(0, margin)
       if j == y[i]:
+        # No need to compute loss if correct class
+        # Also, since both are same => Sij - Sicorrectclassindex + 1 = 0 + 1
+        # => Gradient of loss is 0
+        # Therefore, no need to compute gradient as well
         continue
+
+      # Compute loss
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
+        # Only include loss if more than margin => Loss is not 0
         loss += margin
+        # TEMP TODO FOR REFENCE: dW = np.zeros(W.shape) # (D, C)
+        # Update for wrong class
+        dW[:, j] += X[i]
+        # Update for correct class
+        dW[:, y[i]] -= X[i]
+      else:
+        # If margin < 0 => Max(0, < 0 ) = 0 => loss = 0 => gradient = 0
+        # Thus, don't need to compute loss or gradient
+        continue 
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
+  # Normalize by number of training samples
+  dW /= num_train
 
   # Add regularization to the loss.
   loss += reg * np.sum(W * W)
 
   #############################################################################
-  # TODO:                                                                     #
   # Compute the gradient of the loss function and store it dW.                #
   # Rather that first computing the loss and then computing the derivative,   #
   # it may be simpler to compute the derivative at the same time that the     #
   # loss is being computed. As a result you may need to modify some of the    #
   # code above to compute the gradient.                                       #
   #############################################################################
+  # TODO: 
 
 
   return loss, dW
